160c244442d14d269b53ba8ed3fa4451e429d8bb
Yury Penkov <bsesie71@gmail.com>
2020-12-16 13:40:36 +0300

    add custom logo

--- src/main/java/ru/innopolis/stc/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/ru/innopolis/stc/springbootexample/SpringBootExampleApplication.java
@@ -7,11 +7,19 @@ import org.springframework.context.annotation.Import;
 import ru.innopolis.stc.springbootexample.services.UserService;
 
 @SpringBootApplication
-//@Import({"ru.innopolis.stc.springbootexample.config.MyConfiguration"})
 public class SpringBootExampleApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringBootExampleApplication.class, args);
+    final SpringApplication springApplication = new SpringApplication(SpringBootExampleApplication.class);
+    springApplication.setBanner((environment, sourceClass, out) -> out.println(SLANT_LOGO));
+    springApplication.run(args);
   }
+
+  private static final String SLANT_LOGO =
+        "   _____  ______   ______          _____   ___\n"
+      + "  / ___/ /_  __/  / ____/         |__  /  <  /\n"
+      + "  \\__ \\   / /    / /      ______   /_ <   / / \n"
+      + " ___/ /  / /    / /___   /_____/ ___/ /  / /  \n"
+      + "/____/  /_/     \\____/          /____/  /_/   ";
 
 }
